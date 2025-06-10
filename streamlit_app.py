dffe3909716b67d6c56c9ed05912936894341f34
kbsezginel <kutaybs@gmail.com>
2025-06-10 17:11:50 -0400

    bugfixes, cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import ase.io
 from ase import Atom
-from ase.optimize import BFGS
-from ase.calculators.emt import EMT
-from ase.build import graphene_nanoribbon
 from ase.geometry.analysis import Analysis
 from stmol import showmol, makeobj
 import py3Dmol
@@ -46,7 +43,8 @@ def get_triangles(atoms):
     patoms, bdict = detect_perimeter_by_bonds(atoms, cutoff=2)
     triangles = {}
     for a in patoms:
-        triangles[a] = bdict[a]
+        if len(bdict[a]) == 2:
+            triangles[a] = bdict[a]
     return triangles
 
 def get_lines(atoms):
@@ -58,6 +56,7 @@ def get_lines(atoms):
 
 def add_spokes(atoms):
     triangles = get_triangles(atoms)
+    # st.text(f'Triangles: {len(triangles)} [num atoms: {len(atoms)}]')
     for t in triangles:
         p1 = atoms[t].position
         p2 = atoms[triangles[t][0]].position
@@ -71,6 +70,7 @@ def add_spokes(atoms):
 
 def add_dual_spokes(atoms):
     lines, bdict = get_lines(atoms)
+    # st.text(f'Lines: {len(lines)} [num atoms: {len(atoms)}]')
     for l in lines:
         connected_atom = lines[l][0]
         tri = [i for i in bdict[connected_atom] if i != l]
@@ -105,30 +105,6 @@ def remove_duplicates(atoms, cutoff=0.1):
     # st.text(f'Total: {len(atoms)} Delete: {len(delete)} Keep: {len(keep)}')
     return atoms[keep]
 
-def optimize(atoms):
-    atoms.write('opt.xyz')
-    atoms_new = ase.io.read('opt.xyz')
-    atoms_new.calc = EMT()
-    e_init = atoms_new.get_potential_energy()
-    dyn = BFGS(atoms_new)
-    dyn.run(fmax=0.5)
-    e_final = atoms_new.get_potential_energy()
-    st.text(f'Optimization completed: {e_init:.3f} -> {e_final:.3f}')
-    return atoms_new
-
-def obminimize(atoms, steps=200, ff='mmff94'):
-    atoms.write('tmp.pdb')
-    exe = '/Users/kutay.sezginel/anaconda3/envs/flex/bin/obminimize'
-    cmd = [exe, '-n', str(steps), '-ff', ff, 'tmp.pdb', '>', 'tmp_opt.pdb']
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    with open('tmp_opt.pdb', 'w') as f:
-        f.write(result.stdout)
-    # st.text(result.stdout)
-    atoms = ase.io.read('tmp_opt.pdb')
-    return atoms
-    # obminimize -n 200 -ff mmff94 seed2.pdb > seed2_opt.pdb
-
-
 def spoke(mol):
     mol_new = add_spokes(mol)
     # st.text(f'{len(mol)}')
@@ -139,6 +115,43 @@ def spoke(mol):
     # mol_new = optimize(mol_new)
     return mol_new
 
+def add_perimeter(atoms):
+    ana = Analysis(atoms)
+    ccbonds = ana.get_bonds('C', 'C')
+    st.text(f'Bonds 0 : {len(ccbonds[0])} : {ccbonds[0]}')
+    patoms, bdict = detect_perimeter_by_bonds(atoms, cutoff=2)
+    for a in patoms:
+        if len(bdict[a]) == 1:
+            connected_atom = bdict[a][0]
+            tri = [i for i in bdict[connected_atom] if i != a]
+            p1 = atoms[a].position
+            p2 = atoms[connected_atom].position
+            p3 = atoms[tri[0]].position
+            p4 = atoms[tri[1]].position
+
+            v1 = p1 - p2
+            p3n = p3 + v1 * 2
+            p4n = p4 + v1 * 2
+            atoms.append(Atom('C', p3n))
+            atoms.append(Atom('C', p4n))
+        elif len(bdict[a]) == 2:
+            p1 = atoms[a].position
+            p2 = atoms[bdict[a][0]].position
+            p3 = atoms[bdict[a][1]].position
+            p23 = np.average([p2, p3], axis=0)
+            v1 = p1 - p23
+            pnew = p1 + v1 * 2
+            a = Atom('C', pnew)
+            atoms.append(a)
+    ana = Analysis(atoms)
+    ccbonds = ana.get_bonds('C', 'C')
+    st.text(f'Bonds 1 : {len(ccbonds[0])} : {ccbonds[0]}')
+    atoms = remove_duplicates(atoms, cutoff=0.5)
+    ana = Analysis(atoms)
+    ccbonds = ana.get_bonds('C', 'C')
+    st.text(f'Bonds 2 : {len(ccbonds[0])} : {ccbonds[0]}')
+    atoms = mmff_minimize(atoms)
+    return atoms
 
 
 st.set_page_config(page_title='Perimeter Builder')
@@ -155,13 +168,12 @@ if uploaded_file is not None:
     if str(uploaded_file.name) != st.session_state['filename']:
         st.session_state['count'] = 0
         st.session_state['filename'] = str(uploaded_file.name)
-    # st.text(uploaded_file.name)
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    xyz_str = stringio.read()
-    fname = 'tmppp.xyz'
+    mol_str = stringio.read()
+    mol_ext = uploaded_file.name.split('.')[-1]
+    fname = f'tmppp.{mol_ext}'
     with open(fname, 'w') as f:
-        f.write(xyz_str)
-    # st.write(stringio)
+        f.write(mol_str)
     mol = ase.io.read(fname)
     st.session_state['mol'] = mol
 else:
@@ -176,37 +188,23 @@ cols = st.columns(3)
 # tri_index = cols[0].number_input('Triangle', value=0, min_value=0, placeholder=str(3))
 add_button = cols[0].button('Add Perimeter')
 show_perimeters = cols[1].toggle('Show perimeter atoms', value=True)
-# num = cols[1].number_input('Num', value=1, min_value=0, placeholder=str(3))
-detection_type = cols[2].selectbox('Perimeter Detection', ('Coordination', 'Bonds'))
-
+bg_color = cols[2].color_picker("Background Color", "#ffffff")
 
 # get perimeter atoms
-if detection_type == 'Bonds':
-    patoms, bdict = detect_perimeter_by_bonds(mol)
-elif detection_type == 'Coordination':
-    patoms = detect_perimeter_by_coordination(mol)
-    _, bdict = detect_perimeter_by_bonds(mol)
+patoms, bdict = detect_perimeter_by_bonds(mol)
+
 
 if add_button:
     st.session_state['count'] += 1
     for i in range(st.session_state['count']):
         mol = spoke(mol)
-    # mol = obminimize(mol)
+        # mol = add_perimeter(mol)
+    # mol = mmff_minimize(mol)
     st.session_state['mol'] = mol
-
-    # st.session_state['mol'] = spoke(mol)
-    # st.session_state['mol'] = spoke(st.session_state['mol'])
-    # st.text('SPOKED: ' + str(len(st.session_state['mol'])) + ' ' + str(len(mol)))
 
 st.text('Perimeter Count: ' + str(st.session_state['count']) + '  Num Atoms: ' + str(len(mol)))
 
-
-# tri_str = ' '.join([str(i) for i in triangles])
-# st.text(f'Triangles: {tri_str}')
-# tri_str = ' '.join([str(i) for i in lines])
-# st.text(f'Lines: {tri_str}')
-
-
+# Display moleculer and perimeter atoms
 patoms, bdict = detect_perimeter_by_bonds(st.session_state['mol'])
 
 st.session_state['mol'].write('tmp.pdb')
@@ -215,21 +213,11 @@ pdbfile = open('tmp.pdb').read()
 viewer = py3Dmol.view()
 viewer.addModel(pdbfile,'pdb',{'doAssembly':True,'duplicateAssemblyAtoms':True})
 viewer.setStyle({'sphere':{'scale':.3},'stick':{'colorscheme':'Jmol'}})
+viewer.setBackgroundColor(bg_color)
 
 if show_perimeters:
     for a in patoms:
         viewer.addStyle({'model': -1, 'serial': a + 1}, {'sphere': {'color': 'red', 'radius': 2.}})
-
-# triangles = get_triangles(mol)
-# if tri_index < len(triangles):
-#     t = list(triangles)[tri_index]
-#     for b in triangles[t]:
-#         viewer.addStyle({'model': -1, 'serial': b + 1}, {'sphere': {'color': 'blue', 'radius': 2.}})
-
-
-
-# tri_str = ' '.join([str(i) for i in triangles])
-# st.text(f'Triangles: {tri_str}')
 
 # viewer.setStyle({'sym':2},{'sphere':{'scale':.5,'color':'blue'},'stick':{'color':'cyan'}})
 # viewer.addStyle({'model': -1, 'serial': 1}, {'sphere': {'color': 'red', 'radius': 2.5}})
@@ -254,4 +242,3 @@ st.download_button(
     file_name="mol.xyz",
     icon=":material/download:",
 )
-
