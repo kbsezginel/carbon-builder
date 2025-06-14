edff3532a81775d0e3002408335766af364d7b97
kbsezginel <kutaybs@gmail.com>
2025-06-14 02:56:48 -0400

    test openbabel python 2

--- minimization.py
+++ minimization.py
@@ -86,9 +86,12 @@ def obminimize(pdb_file, steps=20, ff='MMFF94', st=None):
     obConversion.SetInFormat("pdb")
     mol = openbabel.OBMol()
     obConversion.ReadFile(mol, pdb_file)
+    st.text(mol)
 
     ff = openbabel.OBForceField.FindForceField(ff)
     ff.ConjugateGradients(steps)
+    st.text(mol)
+    st.text(ff)
 
     obConversion.WriteFile(mol, "ob_output.pdb")
     
