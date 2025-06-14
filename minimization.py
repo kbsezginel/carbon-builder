27747e2975b7ed77cd10465b53112c53fcfcf61d
kbsezginel <kutaybs@gmail.com>
2025-06-14 04:07:35 -0400

    test minnnnnnnnnnnnnnnnnnnnnnnnnnn

--- minimization.py
+++ minimization.py
@@ -128,6 +128,7 @@ def obminimize(pdb_file, steps=20, ff='MMFF94', st=None):
     ff = openbabel.OBForceField.FindForceField(ff)
     ff.Setup(mol)
     ff.ConjugateGradients(steps)
+    ff.GetCoordinates(mol)
     # ff.SteepestDescent(steps)
     st.text(mol.GetAtom(1).GetVector().GetX())
     st.text(mol.NumAtoms())
